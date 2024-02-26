5ef83fb7b633892e8810064b8b6bcfb03c1a1b45
Muzamil <muzammilmahmood5@gmail.com>
2024-02-26 06:43:56 +0500

    removes auth

--- src/constants/policy.js
+++ src/constants/policy.js
@@ -1,14 +1,14 @@
 const POLICY_NAMES = {
-  INBOUND_CALL_CONVERSION: 'inbound_call_conversion',
-  INBOUND_PLACEMENT_RATE: 'inbound_placement_rate',
-  INBOUND_DAILY_SALES: 'inbound_daily_sales_average',
-  OUTBOUND_CALL_CONVERSION: 'outbound_call_conversion',
-  OUTBOUND_PLACEMENT_RATE: 'outbound_placement_rate',
-  OUTBOUND_DAILY_SALES: 'outbound_daily_sales_average',
-  HRA: 'health_reimbursement_arrangements',
-  VBC: 'value_based_care',
-  REFERRAL_BONUS: 'referral_bonus',
-  POINT_SCALE: 'point_scale',
+  INBOUND_CALL_CONVERSION: "inbound_call_conversion",
+  INBOUND_PLACEMENT_RATE: "inbound_placement_rate",
+  INBOUND_DAILY_SALES: "inbound_daily_sales_average",
+  OUTBOUND_CALL_CONVERSION: "outbound_call_conversion",
+  OUTBOUND_PLACEMENT_RATE: "outbound_placement_rate",
+  OUTBOUND_DAILY_SALES: "outbound_daily_sales_average",
+  HRA: "health_reimbursement_arrangements",
+  VBC: "value_based_care",
+  REFERRAL_BONUS: "referral_bonus",
+  POINT_SCALE: "point_scale",
 };
 
 const THRESHOLD_REQUIRED = [
@@ -23,5 +23,5 @@ const THRESHOLD_REQUIRED = [
 
 module.exports = {
   POLICY_NAMES,
-  THRESHOLD_REQUIRED
+  THRESHOLD_REQUIRED,
 };
--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,21 +1,12 @@
-const { checkAuth } = require('../utils/common.js');
-
 // Authentication for all protected API routes.
 async function authenticate(req, res, next) {
   try {
-    const checksum = await checkAuth(req.body);
-    // Use req.header('authorization') directly and compare without converting to lowercase
-    if (checksum === req.header('authorization')) {
-      next();
-    } else {
-      res.status(403).send('You do not have permission to access this route.');
-    }
+    next();
   } catch (err) {
-    // Pass the error to Express's error handling middleware
     next(err);
   }
 }
 
 module.exports = {
-  authenticate
+  authenticate,
 };
