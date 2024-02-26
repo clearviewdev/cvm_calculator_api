d7475c29c92dd71d6fdd03d1dba30f64295c19eb
Muzamil <muzammilmahmood5@gmail.com>
2024-02-26 22:15:30 +0500

    test auth

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,12 +1,23 @@
+// middleware/auth.js
+
+const { checkAuth } = require('../utils/common.js');
+
 // Authentication for all protected API routes.
 async function authenticate(req, res, next) {
   try {
-    next();
+    const checksum = await checkAuth(req.body);
+    // Use req.header('authorization') directly and compare without converting to lowercase
+	console.log('checksum: ', checksum, "authorization: ", req.header('authorization'))
+    if (checksum === req.header('authorization')) {
+      next();
+    } else {
+      res.status(403).send('You do not have permission to access this route.');
+    }
   } catch (err) {
     next(err);
   }
 }
 
 module.exports = {
-  authenticate,
+  authenticate
 };
